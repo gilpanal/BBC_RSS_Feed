e277b0c2f8a11796df25fe0f909427a24ac585b5
gilpanal <gilpanal@gmail.com>
2016-10-16 22:55:59 +0200

    Last interface modifications
    Icons, load images and banner

---
+++ app/src/main/java/bbcfeed/jose/com/bbc_rss_feed/LazyAdapter.java
@@ -0,0 +1,61 @@
+package bbcfeed.jose.com.bbc_rss_feed;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by josemanuelgil on 16/10/16.
+ */
+public class LazyAdapter extends ArrayAdapter<RSSItem> {
+
+    private static String TAG = "LazyAdapter";
+    private Activity activity;
+    //private ArrayList<HashMap<String, String>> data;
+    private static LayoutInflater inflater=null;
+    public ImageLoader _imageLoader;
+
+
+    public LazyAdapter(Context context, ArrayList<RSSItem> feeds, ImageLoader imageLoader) {
+
+        super(context, 0, feeds);
+        _imageLoader =imageLoader;
+
+    }
+
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+
+        // Get the data item for this position
+        RSSItem feed = getItem(position);
+
+        // Default image - will be shown before loading image
+        int loader = R.drawable.loader;
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_row, parent, false);
+        }
+
+        TextView title = (TextView)convertView.findViewById(R.id.title); // title
+        TextView description = (TextView)convertView.findViewById(R.id.description); // description
+        TextView pubdate = (TextView)convertView.findViewById(R.id.pubdate); // pubdate
+        ImageView thumb_image=(ImageView)convertView.findViewById(R.id.list_image); // thumb image
+
+
+        // Setting all values in listview
+        title.setText(feed.getTitle());
+        description.setText(feed.getDescription());
+        pubdate.setText(feed.getPubDate());
+        _imageLoader.DisplayImage(feed.getThumbnail(),loader,  thumb_image);
+
+        return convertView;
+    }
+}
--- app/src/main/java/bbcfeed/jose/com/bbc_rss_feed/RSSReader.java
+++ app/src/main/java/bbcfeed/jose/com/bbc_rss_feed/RSSReader.java
@@ -3,15 +3,12 @@ package bbcfeed.jose.com.bbc_rss_feed;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -19,6 +16,7 @@ import org.xml.sax.InputSource;
 import org.xml.sax.XMLReader;
 
 import java.net.URL;
+import java.util.ArrayList;
 
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
@@ -35,16 +33,17 @@ public class RSSReader extends AppCompatActivity implements AdapterView.OnItemCl
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         new RetrieveFeedTask().execute(RSSFEEDOFCHOICE);
     }
@@ -86,6 +85,8 @@ public class RSSReader extends AppCompatActivity implements AdapterView.OnItemCl
         TextView feedpubdate = (TextView) findViewById(R.id.feedpubdate);
         ListView itemlist = (ListView) findViewById(R.id.itemlist);
 
+        // ImageLoader class instance
+        ImageLoader imgLoader = new ImageLoader(getApplicationContext());
 
         if (feed == null)
         {
@@ -101,7 +102,12 @@ public class RSSReader extends AppCompatActivity implements AdapterView.OnItemCl
         feedtitle.setText(feed.getTitle());
         feedpubdate.setText(feed.getPubDate());
 
-        ArrayAdapter<RSSItem> adapter = new ArrayAdapter<RSSItem>(this,android.R.layout.simple_list_item_1,feed.getAllItems());
+        //ArrayAdapter<RSSItem> adapter = new ArrayAdapter<RSSItem>(this,android.R.layout.simple_list_item_1,feed.getAllItems());
+
+        ArrayList<RSSItem> arrayOfFeeds = new ArrayList<RSSItem>(feed.getAllItems());
+
+        // Create the adapter to convert the array to views
+        LazyAdapter adapter = new LazyAdapter(this, arrayOfFeeds, imgLoader);
 
         itemlist.setAdapter(adapter);
 
@@ -144,9 +150,13 @@ public class RSSReader extends AppCompatActivity implements AdapterView.OnItemCl
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        //if (id == R.id.action_settings) {
+        //    return true;
+        //}
+        if (id == R.id.action_search) {
             return true;
         }
+
 
         return super.onOptionsItemSelected(item);
     }
